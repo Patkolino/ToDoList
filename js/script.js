5b17c5102d702b84ed1fd4aecba9c1cc3ebc6a8e
Patrycja K <patrycjakostyniak@gmail.com>
2025-04-09 18:31:07 +0200

    add new task and remove task

--- js/script.js
+++ js/script.js
@@ -10,22 +10,57 @@
         },
     ];
 
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
                <li>
+                  <button class="js-remove">Remove task</button>
                   ${task.content}
                </li>
             `;
         }
 
         document.querySelector(".js-taskList").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
     };
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
